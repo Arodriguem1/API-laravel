ed67eac9b36e2341fc6aa376c8e58e19242291ea
Arodriguem1 <Arodriguem1@gmail.com>
2024-04-22 14:39:44 -0500

    try

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,15 +3,8 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Tests\CreatesApplication; 
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
-
-    protected function setUp():void
-    {
-        parent::setUp();
-        $this->withoutVite();
-    }
+    
 }
